e6373eaa091a6f9ea5e808962737274315f5be7e
HurryWang <yuyuqaq@foxmail.com>
2026-04-01 16:13:30 +0800

    feat: 完善日志输出

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const rateLimit = require("express-rate-limit");
 const morgan = require("morgan");
 const cors = require("cors");
+const dayjs = require("dayjs");
 const config = require("./config/config");
 const lanzouRouter = require("./routes/lanzou");
 
@@ -11,7 +12,14 @@ app.set("trust proxy", "loopback");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan("tiny"));
+
+morgan.token("time", () => dayjs().format("YYYY-MM-DD HH:mm:ss"));
+app.use(
+  morgan(
+    "[:time] :method :url :status :res[content-length] - :response-time ms",
+  ),
+);
+
 app.use(
   cors({
     allowedHeaders: "*",
@@ -24,12 +32,17 @@ app.use(rateLimit(config.rateLimit));
 app.use("/lanzou", lanzouRouter);
 
 app.use((err, req, res, next) => {
-  console.error("🔥 [Unhandled Error]", err);
+  console.error(
+    `[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] 🔥 [Unhandled Error]`,
+    err,
+  );
   res.status(500).json({ error: "Unexpected server error" });
 });
 
 app.listen(config.PORT, () => {
-  console.log(`Server running at http://127.0.0.1:${config.PORT}`);
+  console.log(
+    `[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] Server running at http://127.0.0.1:${config.PORT}`,
+  );
 });
 
 module.exports = app;
